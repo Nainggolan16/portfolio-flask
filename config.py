09d2d75b5980849dfd177c6b1207756afe794063
Nico Saputra Nainggolan <682024109@student.uksw.edu>
2026-07-01 20:11:12 +0700

    Add TiDB SSL certificate

--- config.py
+++ config.py
@@ -3,18 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    # Database
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "ssl": {
+                "ca": "certs/isrgrootx1.pem"
+            }
+        }
+    }
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Cloudinary
     CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
     CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
     CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
-    # Resend
     RESEND_API_KEY = os.getenv("RESEND_API_KEY")
